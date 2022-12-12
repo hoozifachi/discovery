ebab73c95510c610b9a05a70b1da2085518d10e9
Jason Sweet <hoozifachi@gmail.com>
2022-12-11 21:52:40 -0600

    Shows ODR changes when you write to BSR.

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -5,13 +5,47 @@ use core::ptr;
 
 #[allow(unused_imports)]
 use aux7::entry;
+use aux7::{iprintln, ITM};
+
+// Print the current content of odr
+fn iprint_odr(itm: &mut ITM) {
+    const GPIOE_ODR: u32 = 0x4800_1014;
+
+    unsafe {
+        iprintln!(
+            &mut itm.stim[0],
+            "ODR = 0x{:04x}",
+            ptr::read(GPIOE_ODR as *const i16)
+        );
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    aux7::init();
+    let mut itm = aux7::init().0;
 
     unsafe {
-        ptr::read_volatile(0x4800_1800 as *const u32);
+        // A magic addess
+        const GPIOE_BSSR: u32 = 0x4800_1018;
+
+        // Print the initial contents of ODR
+        iprint_odr(&mut itm);
+
+        // turn on the "North" LED (red)
+        ptr::write_volatile(GPIOE_BSSR as *mut u32, 1 << 9);
+        iprint_odr(&mut itm);
+
+        // turn on the "East" LED (green)
+        ptr::write_volatile(GPIOE_BSSR as *mut u32, 1 << 11);
+        iprint_odr(&mut itm);
+
+        // turn off the "North" LED (red)
+        ptr::write_volatile(GPIOE_BSSR as *mut u32, 1 << (9 + 16));
+        iprint_odr(&mut itm);
+
+        // turn off the "East" LED (green)
+        ptr::write_volatile(GPIOE_BSSR as *mut u32, 1 << (11 + 16));
+        iprint_odr(&mut itm);
     }
 
     loop {}
