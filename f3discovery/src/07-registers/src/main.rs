b62ac57cd14afda33e41689f6195835acabe2ef8
Jason Sweet <hoozifachi@gmail.com>
2022-12-11 21:37:59 -0600

    Reads invalid register.

--- f3discovery/src/07-registers/src/main.rs
+++ f3discovery/src/07-registers/src/main.rs
@@ -11,20 +11,7 @@ fn main() -> ! {
     aux7::init();
 
     unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
-
-        // Turn on the "North" LED (red)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
-
-        // Turn on the "East" LED (green)
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
-
-        // Turn off the "North" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
-
-        // Turn off the "East" LED
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+        ptr::read_volatile(0x4800_1800 as *const u32);
     }
 
     loop {}
