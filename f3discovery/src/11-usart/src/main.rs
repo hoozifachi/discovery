53fcdf2bf527d41978acd8454997747150aa2ac1
Jason Sweet <hoozifachi@gmail.com>
2022-12-15 05:45:09 -0600

    Slows down sending bytes to prevent buffer overrun.

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -6,14 +6,25 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, _mono_timer, _itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    let str = "The quick brown fox jumps over the lazy dog.";
+    let str = b"The quick brown fox jumps over the lazy dog.";
 
-    for c in str.bytes() {
+    let instant = mono_timer.now();
+    for byte in str.iter() {
+        while usart1.isr.read().txe().bit_is_clear() {}
+
         // Send a single character
-        usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
     }
+    let elapsed = instant.elapsed(); // in ticks
+
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
 
     loop {}
 }
