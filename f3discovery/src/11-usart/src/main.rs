3db4d76c200388127aed1932dd1263e86cfb2bf1
Jason Sweet <hoozifachi@gmail.com>
2022-12-15 05:28:04 -0600

    Writes string to USART.

--- f3discovery/src/11-usart/src/main.rs
+++ f3discovery/src/11-usart/src/main.rs
@@ -8,10 +8,12 @@ use aux11::{entry, iprint, iprintln};
 fn main() -> ! {
     let (usart1, _mono_timer, _itm) = aux11::init();
 
-    // Send a single character
-    usart1
-        .tdr
-        .write(|w| w.tdr().bits(u16::from(b'X')) );
+    let str = "The quick brown fox jumps over the lazy dog.";
+
+    for c in str.bytes() {
+        // Send a single character
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(c)));
+    }
 
     loop {}
 }
